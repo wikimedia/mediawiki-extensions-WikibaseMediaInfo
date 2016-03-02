51db8271fb92f64944e045568f77621349a8b682
Jan Zerebecki <jan.wikimedia@zerebecki.de>
2016-03-02 17:28:21 +0100

    Initial extension code adapted from BoilerPlate
    Change-Id: Ia76badd45d76cde4a0a68b18cdbf04bc11c09df5

---
+++ WikibaseMediaInfo.i18n.alias.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Aliases for special pages of the WikibaseMediaInfo extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+];

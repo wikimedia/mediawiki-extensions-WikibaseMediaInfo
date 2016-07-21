0f55e1162c0534263bf909e0ada79421e83af7df
daniel <daniel.kinzler@wikimedia.de>
2016-07-21 15:35:04 +0200

    Add link to file page to MediaInfoView.
    Basline implementation, not pretty.
    
    Bug: T134260
    Change-Id: Ice8123402c7e38c312fa5fb701c30bd806e6941a

--- WikibaseMediaInfo.entitytypes.php
+++ WikibaseMediaInfo.entitytypes.php
@@ -13,6 +13,7 @@
  * @author Bene* < benestar.wikimedia@gmail.com >
  */
 
+use MediaWiki\MediaWikiServices;
 use Wikibase\DataModel\DeserializerFactory;
 use Wikibase\DataModel\SerializerFactory;
 use Wikibase\DataModel\Services\Lookup\LabelDescriptionLookup;
@@ -67,7 +68,9 @@ return [
 					$editSectionGenerator
 				),
 				new MediaWikiLanguageDirectionalityLookup(),
-				$languageCode
+				$languageCode,
+				MediaWikiServices::getInstance()->getLinkRenderer(),
+				MediaInfoServices::getFilePageLookup()
 			);
 		},
 		'content-model-id' => MediaInfoContent::CONTENT_MODEL_ID,

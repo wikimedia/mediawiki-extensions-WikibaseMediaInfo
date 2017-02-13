c379c765f9518a4b6e05ce10b714df1e9faca008
aude <aude.wiki@gmail.com>
2017-02-12 22:08:34 -0500

    Use extension registration for namespaces
    Instead of SetupAfterCache hook, which is hacky.
    
    For now, this removes the option to disable automatic
    namespace registration by setting MediaInfoNamespace
    to false.
    
    T152582 would allow customization in the future
    of the namespace registrations in LocalSettings.php
    
    Bug: T157919
    Change-Id: I6f47ee0cf2d96363405613600c087250884aed1e

--- src/WikibaseMediaInfoHooks.php
+++ src/WikibaseMediaInfoHooks.php
@@ -29,35 +29,6 @@ class WikibaseMediaInfoHooks {
 	}
 
 	/**
-	 * Hook to register the default namespace names with $wgExtraNamespaces.
-	 *
-	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/SetupAfterCache
-	 */
-	public static function onSetupAfterCache() {
-		global $wgExtraNamespaces;
-
-		// XXX: ExtensionProcessor should define an extra config object for every extension.
-		$config = MediaWikiServices::getInstance()->getMainConfig();
-
-		// Setting the namespace to false disabled automatic registration.
-		$entityNamespace = $config->get( 'MediaInfoNamespace' );
-		$talkNamespace = $config->get( 'MediaInfoTalkNamespace' );
-
-		if ( $entityNamespace !== false ) {
-			if ( !isset( $wgExtraNamespaces[$entityNamespace] ) && $entityNamespace >= 100 ) {
-				$wgExtraNamespaces[$entityNamespace] = 'MediaInfo';
-			}
-		}
-
-		if ( $talkNamespace !== false ) {
-			if ( !isset( $wgExtraNamespaces[$talkNamespace] ) && $entityNamespace >= 100 ) {
-				// XXX: Localize the default talk namespace?
-				$wgExtraNamespaces[$talkNamespace] = 'MediaInfo_Talk';
-			}
-		}
-	}
-
-	/**
 	 * Adds the definition of the media info entity type to the definitions array Wikibase uses.
 	 *
 	 * @see WikibaseMediaInfo.entitytypes.php

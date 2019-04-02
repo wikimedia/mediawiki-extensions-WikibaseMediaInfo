98f8399d9196a524fbfaabdd99292e7a429dc24e
Cormac Parle <cparle@wikimedia.org>
2019-04-02 16:51:39 +0100

    Put panel initialisation inside hooks
    The panels needs to be initialized inside hooks so
    they can be reconstructed if the content is reloaded
    by an extension
    
    Bug: T218922
    Change-Id: Idd5ac5d8a2ef09375481fc54723c1b1e3df3fc6d

--- resources/filepage/init.js
+++ resources/filepage/init.js
@@ -18,42 +18,45 @@
 	CaptionsPanel = require( './CaptionsPanel.js' );
 	DepictsPanel = require( './DepictsPanel.js' );
 
-	if (
-		// make sure there's a statements block on the page (e.g. if it's feature-flagged off)
-		$( '.' + depictsClass ).length !== 0 &&
-		// and we have an ID for "depicts" in config
-		'depicts' in mw.config.get( 'wbmiProperties' )
-	) {
-		// eslint-disable-next-line no-jquery/no-global-selector
-		$tabs = $( '.wbmi-tabs' );
+	// This has to go inside hooks to allow proper creation of js elements when content is
+	// replaced by the RevisionSlider extension
+	mw.hook( 'wikipage.content' ).add( function ( $content ) {
 
-		if ( $tabs.length > 0 ) {
-			OO.ui.infuse( $tabs );
+		captions = new CaptionsPanel( {
+			headerClass: 'wbmi-entityview-captions-header',
+			contentClass: 'wbmi-entityview-captionsPanel',
+			entityTermClass: 'wbmi-entityview-caption',
+			warnWithinMaxCaptionLength: 20,
+			// .emptyEntity was previously used, and will be fallback value, for cached output
+			// this can be removed a month after getting being merged...
+			// eslint-disable-next-line no-jquery/no-global-selector
+			captionsExist: mw.config.get( 'wbmiCaptionsExist', $( '.emptyEntity' ).length > 0 )
+		} );
+
+		captions.initialize();
+
+		if (
+			// make sure there's a statements block on the page (e.g. if it's feature-flagged off)
+			$content.find( '.' + depictsClass ).length !== 0 &&
+			// and we have an ID for "depicts" in config
+			'depicts' in mw.config.get( 'wbmiProperties' )
+		) {
+			$tabs = $content.find( '.wbmi-tabs' );
+
+			if ( $tabs.length > 0 ) {
+				OO.ui.infuse( $tabs );
+			}
+
+			// Only allow editing if we're NOT on a diff page or viewing an older revision
+			if ( $content.find( '.diff' ).length === 0 && $content.find( '.mw-revision' ).length === 0 ) {
+				depicts = new DepictsPanel( {
+					contentClass: depictsClass,
+					entityId: mw.config.get( 'wbEntityId' )
+				} );
+
+				depicts.initialize();
+			}
 		}
-
-		// Only allow editing if we're NOT on a diff page or viewing an older revision
-		// eslint-disable-next-line no-jquery/no-global-selector
-		if ( $( '.diff' ).length === 0 && $( '.mw-revision' ).length === 0 ) {
-			depicts = new DepictsPanel( {
-				contentClass: depictsClass,
-				entityId: mw.config.get( 'wbEntityId' )
-			} );
-
-			depicts.initialize();
-		}
-	}
-
-	captions = new CaptionsPanel( {
-		headerClass: 'wbmi-entityview-captions-header',
-		contentClass: 'wbmi-entityview-captionsPanel',
-		entityTermClass: 'wbmi-entityview-caption',
-		warnWithinMaxCaptionLength: 20,
-		// .emptyEntity was previously used, and will be fallback value, for cached output
-		// this can be removed a month after getting being merged...
-		// eslint-disable-next-line no-jquery/no-global-selector
-		captionsExist: mw.config.get( 'wbmiCaptionsExist', $( '.emptyEntity' ).length > 0 )
 	} );
-
-	captions.initialize();
 
 }() );

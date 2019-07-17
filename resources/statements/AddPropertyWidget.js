52ec0a93fdbf265b69e76aff07a600caadf4d126
Matthias Mullie <git@mullie.eu>
2019-07-17 14:53:53 +0200

    Convert AddPropertyWidget to use templates
    Change-Id: I919ed76321eff5e0631a46b10e6f226fd417201a

--- resources/statements/AddPropertyWidget.js
+++ resources/statements/AddPropertyWidget.js
@@ -10,62 +10,80 @@ var ItemInputWidget = require( './ItemInputWidget.js' ),
 		config = config || {};
 		AddPropertyWidget.parent.call(
 			this,
-			$.extend( config, { classes: [ 'wbmi-add-property' ] } )
+			config
 		);
 
 		this.propertyIds = config.propertyIds || [];
+		this.editing = false;
 
-		this.addPropertyButton = new OO.ui.ButtonWidget( {
-			classes: [ 'wbmi-entityview-add-statement-property-button' ],
-			framed: true,
-			icon: 'add',
-			flags: [ 'progressive' ],
-			label: mw.message( 'wikibasemediainfo-add-statement' ).text()
-		} );
-		this.addPropertyButton.connect( this, { click: 'onClick' } );
-
-		this.inputContainer = new OO.ui.Widget( {
-			classes: [ 'wbmi-entityview-add-statement-property-container' ]
-		} );
-		this.propertyInputWidget = new ItemInputWidget( {
-			classes: [ 'wbmi-entityview-add-statement-property' ],
-			entityType: 'property',
-			filter: this.getFilters(),
-			maxSuggestions: 7,
-			placeholder: mw.message( 'wikibasemediainfo-add-property' ).text()
-		} );
-		this.propertyInputWidget.connect( this, { choose: 'onChoose' } );
-		this.propertyInputWidget.connect( this, { choose: [ 'emit', 'choose' ] } );
-
-		this.removeButton = new OO.ui.ButtonWidget( {
-			classes: [ 'wbmi-item-remove' ],
-			title: mw.message( 'wikibasemediainfo-statements-item-remove' ).text(),
-			flags: 'destructive',
-			icon: 'trash',
-			framed: false
-		} );
-		this.removeButton.connect( this, { click: 'onClick' } );
-		this.inputContainer.$element.append(
-			this.propertyInputWidget.$element,
-			this.removeButton.$element
-		);
-
-		this.$element.append(
-			this.addPropertyButton.$element,
-			this.inputContainer.$element.hide()
-		);
+		this.render();
 	};
 OO.inheritClass( AddPropertyWidget, OO.ui.Widget );
+
+AddPropertyWidget.prototype.render = function () {
+	var data,
+		template,
+		$element,
+		propertyInputWidget,
+		addPropertyButton,
+		removeButton;
+
+	propertyInputWidget = new ItemInputWidget( {
+		classes: [ 'wbmi-entityview-add-statement-property' ],
+		entityType: 'property',
+		filter: this.getFilters(),
+		maxSuggestions: 7,
+		placeholder: mw.message( 'wikibasemediainfo-add-property' ).text()
+	} );
+	propertyInputWidget.connect( this, { choose: 'onChoose' } );
+	propertyInputWidget.connect( this, { choose: [ 'setEditing', false ] } );
+	propertyInputWidget.connect( this, { choose: [ 'emit', 'choose' ] } );
+
+	addPropertyButton = new OO.ui.ButtonWidget( {
+		classes: [ 'wbmi-entityview-add-statement-property-button' ],
+		framed: true,
+		icon: 'add',
+		flags: [ 'progressive' ],
+		label: mw.message( 'wikibasemediainfo-add-statement' ).text()
+	} );
+	addPropertyButton.connect( this, { click: [ 'setEditing', !this.editing ] } );
+
+	removeButton = new OO.ui.ButtonWidget( {
+		classes: [ 'wbmi-item-remove' ],
+		title: mw.message( 'wikibasemediainfo-statements-item-remove' ).text(),
+		flags: 'destructive',
+		icon: 'trash',
+		framed: false
+	} );
+	removeButton.connect( this, { click: [ 'setEditing', false ] } );
+
+	template = mw.template.get(
+		'wikibase.mediainfo.statements',
+		'templates/statements/AddPropertyWidget.mustache+dom'
+	);
+
+	data = {
+		editing: this.editing,
+		addPropertyButton: addPropertyButton,
+		propertyInputWidget: propertyInputWidget,
+		removeButton: removeButton
+	};
+
+	$element = template.render( data );
+	this.$element.empty().append( $element );
+};
+
+AddPropertyWidget.prototype.setEditing = function ( editing ) {
+	if ( this.editing !== editing ) {
+		this.editing = editing;
+		this.render();
+	}
+};
 
 AddPropertyWidget.prototype.addPropertyId = function ( propertyId ) {
 	if ( this.propertyIds.indexOf( propertyId ) === -1 ) {
 		this.propertyIds.push( propertyId );
 	}
-	this.propertyInputWidget.setFilter( this.getFilters() );
-};
-
-AddPropertyWidget.prototype.onClick = function () {
-	this.inputContainer.$element.toggle();
 };
 
 /**
@@ -73,11 +91,7 @@ AddPropertyWidget.prototype.onClick = function () {
  * @param {Object} data
  */
 AddPropertyWidget.prototype.onChoose = function ( item, data ) {
-	this.propertyInputWidget.setValue( '' );
-	this.inputContainer.$element.hide();
-
 	this.propertyIds.push( data.id );
-	this.propertyInputWidget.setFilter( this.getFilters() );
 };
 
 /**
@@ -101,7 +115,6 @@ AddPropertyWidget.prototype.onStatementPanelRemoved = function ( panelPropertyId
 	this.propertyIds = this.propertyIds.filter( function ( propertyId ) {
 		return propertyId !== panelPropertyId;
 	} );
-	this.propertyInputWidget.setFilter( this.getFilters() );
 };
 
 module.exports = AddPropertyWidget;

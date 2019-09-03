a9ed9d0c1c14e4e2b0fc820357f65848d083b08b
Matthias Mullie <git@mullie.eu>
2019-09-03 13:07:34 +0200

    Add more tests for expected QualifierValueInputWidget behavior
    Change-Id: I5b70878b876b4016e1c7f6891542aaaac2e49a83

--- tests/node-qunit/mediainfo/statements/QualifierValueInputWidget.test.js
+++ tests/node-qunit/mediainfo/statements/QualifierValueInputWidget.test.js
@@ -1,4 +1,5 @@
-var pathToWidget = '../../../../resources/statements/QualifierValueInputWidget.js',
+var sinon = require( 'sinon' ),
+	pathToWidget = '../../../../resources/statements/QualifierValueInputWidget.js',
 	hooks = require( '../../support/hooks.js' );
 
 QUnit.module( 'QualifierValueInputWidget', hooks.mediainfo, function () {
@@ -65,5 +66,68 @@ QUnit.module( 'QualifierValueInputWidget', hooks.mediainfo, function () {
 			assert.strictEqual( data.equals( widget.getData() ), true );
 			done();
 		} );
+	} );
+
+	QUnit.test( 'Setting other data triggers a change event', function ( assert ) {
+		var done = assert.async(),
+			QualifierValueInputWidget = require( pathToWidget ),
+			widget = new QualifierValueInputWidget(),
+			data = new dataValues.StringValue( 'this is a test' ),
+			newData = new dataValues.StringValue( 'this is a change' ),
+			onChange = sinon.stub();
+
+		widget.setData( data )
+			.then( widget.on.bind( widget, 'change', onChange, [] ) )
+			.then( widget.setData.bind( widget, newData ) )
+			.then( function () {
+				assert.strictEqual( onChange.called, true );
+				done();
+			} );
+	} );
+
+	QUnit.test( 'Setting same data does not trigger a change event', function ( assert ) {
+		var done = assert.async(),
+			QualifierValueInputWidget = require( pathToWidget ),
+			widget = new QualifierValueInputWidget(),
+			data = new dataValues.StringValue( 'this is a test' ),
+			sameData = new dataValues.StringValue( 'this is a test' ),
+			onChange = sinon.stub();
+
+		widget.setData( data )
+			.then( widget.on.bind( widget, 'change', onChange, [] ) )
+			.then( widget.setData.bind( widget, sameData ) )
+			.then( function () {
+				assert.strictEqual( onChange.called, false );
+				done();
+			} );
+	} );
+
+	QUnit.test( 'Changing to same input type leaves existing value unaltered', function ( assert ) {
+		var done = assert.async(),
+			QualifierValueInputWidget = require( pathToWidget ),
+			widget = new QualifierValueInputWidget(),
+			data = new dataValues.StringValue( 'this is a test' );
+
+		widget.setData( data )
+			.then( widget.setInputType.bind( widget, 'string' ) )
+			.then( function () {
+				assert.strictEqual( data.equals( widget.getData() ), true );
+				done();
+			} );
+	} );
+
+	QUnit.test( 'Changing to other input type (and back) wipes out existing data', function ( assert ) {
+		var done = assert.async(),
+			QualifierValueInputWidget = require( pathToWidget ),
+			widget = new QualifierValueInputWidget(),
+			data = new dataValues.StringValue( 'this is a test' );
+
+		widget.setData( data )
+			.then( widget.setInputType.bind( widget, 'quantity' ) )
+			.then( widget.setInputType.bind( widget, 'string' ) )
+			.then( function () {
+				assert.strictEqual( data.equals( widget.getData() ), false );
+				done();
+			} );
 	} );
 } );

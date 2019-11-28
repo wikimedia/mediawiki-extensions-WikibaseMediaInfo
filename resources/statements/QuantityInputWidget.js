a54812813e68e0b3ecc55a13ac8ccbd5549fe570
Matthias Mullie <git@mullie.eu>
2019-11-28 14:57:37 +0100

    Add statement-level support for quantity input
    Since quantity inputs now need to be displayed differently
    depending on their context (qualifier vs top level), this
    element has now also become a template-based widget.
    
    Bug: T231981
    Change-Id: Icf1e7c35ae743f75fca40f6de30c636499813c69

--- resources/statements/QuantityInputWidget.js
+++ resources/statements/QuantityInputWidget.js
@@ -1,21 +1,88 @@
 'use strict';
 
-var QuantityInputWidget;
+var ComponentWidget = require( 'wikibase.mediainfo.base' ).ComponentWidget,
+	QuantityInputWidget;
 
 /**
- * @constructor
- * @param {Object} config
+ * @param {Object} config Configuration options
+ * @param {number} [config.value]
+ * @param {boolean} [config.isQualifier]
  */
-QuantityInputWidget = function ( config ) {
-	this.config = $.extend( {}, config );
-	QuantityInputWidget.parent.call( this, this.config );
-	this.connect( this, { enter: 'onEnter' } );
+QuantityInputWidget = function MediaInfoStatementsQuantityInputWidget( config ) {
+	config = config || {};
+
+	this.state = {
+		value: config.value || '',
+		isQualifier: !!config.isQualifier
+	};
+
+	this.input = new OO.ui.NumberInputWidget( {
+		value: this.state.value,
+		classes: [ 'wbmi-quantity-input-input' ],
+		isRequired: true
+	} );
+	this.input.connect( this, { enter: 'onEnter' } );
+	this.input.connect( this, { change: 'onChange' } );
+
+	QuantityInputWidget.parent.call( this );
+	ComponentWidget.call(
+		this,
+		'wikibase.mediainfo.statements',
+		'templates/statements/QuantityInputWidget.mustache+dom'
+	);
+};
+OO.inheritClass( QuantityInputWidget, OO.ui.Widget );
+OO.mixinClass( QuantityInputWidget, ComponentWidget );
+
+/**
+ * @inheritDoc
+ */
+QuantityInputWidget.prototype.getTemplateData = function () {
+	var button = new OO.ui.ButtonWidget( {
+		classes: [ 'wbmi-quantity-input-button' ],
+		label: mw.message( 'wikibasemediainfo-quantity-input-button-text' ).text(),
+		flags: [ 'primary', 'progressive' ],
+		disabled: this.input.getValue() === ''
+	} );
+	button.connect( this, { click: 'onEnter' } );
+
+	return {
+		isQualifier: this.state.isQualifier,
+		input: this.input,
+		button: button
+	};
 };
 
-OO.inheritClass( QuantityInputWidget, OO.ui.NumberInputWidget );
-
 QuantityInputWidget.prototype.onEnter = function () {
-	this.emit( 'addItem', this.value );
+	this.emit( 'addItem', this.input.getValue() );
+};
+
+QuantityInputWidget.prototype.onChange = function () {
+	// update state to make sure template rerenders
+	this.setState( { value: this.input.getValue() } )
+		.then( this.emit.bind( this, 'change', this.input.getValue() ) );
+};
+
+/**
+ * @return {Object}
+ */
+QuantityInputWidget.prototype.getData = function () {
+	return {
+		// add leading '+' if no unit is present already
+		amount: this.input.getValue().replace( /^(?![+-])/, '+' ),
+		unit: '1'
+	};
+};
+
+/**
+ * @param {Object} data
+ * @return {jQuery.Promise}
+ */
+QuantityInputWidget.prototype.setData = function ( data ) {
+	// replace leading '+' unit - that's only needed for internal storage,
+	// but obvious for human input
+	this.input.setValue( data.amount.replace( /^\+/, '' ) );
+	return this.setState( { value: this.input.getValue() } );
 };
 
 module.exports = QuantityInputWidget;

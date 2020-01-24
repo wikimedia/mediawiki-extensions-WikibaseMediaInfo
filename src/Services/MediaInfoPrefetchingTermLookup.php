0dec146978397d735ea736604a1193592e897664
addshore <addshorewiki@gmail.com>
2020-01-24 15:25:01 +0100

    Remove TermBuffer compat for data-model-services change
    Bug: T243596
    Change-Id: Ide9361226d6858e14a7ae6ad482ff332c9e2f5d2
    Depends-On: Ie52d6874543aa4c19dd356d0a1f61684a9abfe13

--- src/Services/MediaInfoPrefetchingTermLookup.php
+++ src/Services/MediaInfoPrefetchingTermLookup.php
@@ -88,18 +88,9 @@ class MediaInfoPrefetchingTermLookup extends EntityTermLookupBase implements Pre
 	}
 
 	/**
-	 * @todo $termTypes and $languageCodes can not be null with data-model-service ~5.0
-	 * Code calling this already always passes array here and the defaults should be removed soon
-	 * Leaving the defaults in this method allows us to stay compatible with ~4.0 and ~5.0
-	 * for a short period during migration and updates.
-	 *
 	 * @inheritDoc
 	 */
-	public function prefetchTerms( array $entityIds, array $termTypes = null, array $languageCodes = null ) {
-		if ( $termTypes === null || $languageCodes === null ) {
-			throw new \InvalidArgumentException( '$termTypes and $languageCodes can not be null' );
-		}
-
+	public function prefetchTerms( array $entityIds, array $termTypes, array $languageCodes ) {
 		foreach ( $entityIds as $entityId ) {
 			$serialization = $entityId->getSerialization();
 			if ( isset( $this->terms[$serialization] ) && $this->terms[$serialization] === false ) {

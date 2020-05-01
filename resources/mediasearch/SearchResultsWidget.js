3df893615b55bd632284c441c479b71956751060
Matthias Mullie <git@mullie.eu>
2020-05-01 13:37:11 +0200

    Pretend to have concept chips for a few terms
    This hardcodes a few concept chips, for:
    - redwood & redwood tree
    - new york & new york city
    - van gogh & vincent van gogh
    
    The suggested chips were based on Google Image's suggestions.
    Thumbnails are the first results for 'term + chip' searches
    on Commons. Some of those don't make too much sense (or rather,
    are not the perfect result - i.e. van gogh + sunflower) and
    some are duplicated (i.e. van gogh + vincent = van gogh + paintings)
    But that'll be reality... There's no point in cherry-picking
    perfect examples to create an unrealistic test environment.
    
    In order to make it look like an actual thing, clicking the
    concept chips will lead you to "term + chip" search results.
    We don't know whether they'll actually behave like this in
    the end, but at least they seem to be doing something useful
    enough during research.
    
    Bug: T249614
    Change-Id: I776b777f2dd76ff313ee105d303523611acff0aa

--- resources/mediasearch/SearchResultsWidget.js
+++ resources/mediasearch/SearchResultsWidget.js
@@ -56,7 +56,143 @@ SearchResultsWidget.prototype.getTemplateData = function () {
 		isBitmap: this.state.type === 'bitmap',
 		isAudio: this.state.type === 'audio',
 		isVideo: this.state.type === 'video',
-		isCategory: this.state.type === 'category'
+		isCategory: this.state.type === 'category',
+
+		// @todo below is for testing - this should be removed once the testing period is over!
+		chips: {
+			redwood: [
+				{
+					title: 'California',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Giant_Redwood_Trees_in_California_that_Inspired_a_Film_and_Video_game.jpg/40px-Giant_Redwood_Trees_in_California_that_Inspired_a_Film_and_Video_game.jpg'
+				},
+				{
+					title: 'Giant',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Redwood_large_sherman.jpg/40px-Redwood_large_sherman.jpg'
+				},
+				{
+					title: 'Drive thru',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Delorean_at_Redwood_%2817033306994%29.jpg/40px-Delorean_at_Redwood_%2817033306994%29.jpg'
+				},
+				{
+					title: 'Chandelier',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/CarRedwoodLeggett.jpg/40px-CarRedwoodLeggett.jpg'
+				},
+				{
+					title: 'Giant sequoias',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Redwood_large_sherman.jpg/40px-Redwood_large_sherman.jpg'
+				}
+			],
+			'redwood tree': [
+				{
+					title: 'California',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Giant_Redwood_Trees_in_California_that_Inspired_a_Film_and_Video_game.jpg/40px-Giant_Redwood_Trees_in_California_that_Inspired_a_Film_and_Video_game.jpg'
+				},
+				{
+					title: 'Giant',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Redwood_large_sherman.jpg/40px-Redwood_large_sherman.jpg'
+				},
+				{
+					title: 'Drive thru',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Delorean_at_Redwood_%2817033306994%29.jpg/40px-Delorean_at_Redwood_%2817033306994%29.jpg'
+				},
+				{
+					title: 'Chandelier',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/CarRedwoodLeggett.jpg/40px-CarRedwoodLeggett.jpg'
+				},
+				{
+					title: 'Giant sequoias',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Redwood_large_sherman.jpg/40px-Redwood_large_sherman.jpg'
+				}
+			],
+			'new york': [
+				{
+					title: 'Times Square',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/New_York_City_%28New_York%2C_USA%29%2C_Times_Square-Duffy_Square_--_2012_--_6380.jpg/40px-New_York_City_%28New_York%2C_USA%29%2C_Times_Square-Duffy_Square_--_2012_--_6380.jpg'
+				},
+				{
+					title: 'NYC',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Washington_Crossing_the_Delaware_by_Emanuel_Leutze%2C_MMA-NYC%2C_1851.jpg/40px-Washington_Crossing_the_Delaware_by_Emanuel_Leutze%2C_MMA-NYC%2C_1851.jpg'
+				},
+				{
+					title: 'Manhattan',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/New_York_City_at_night_HDR.jpg/40px-New_York_City_at_night_HDR.jpg'
+				},
+				{
+					title: 'High line',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/London_Terrace_NY1.jpg/40px-London_Terrace_NY1.jpg'
+				},
+				{
+					title: 'Hotel',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/New_York_City_at_night_HDR.jpg/40px-New_York_City_at_night_HDR.jpg'
+				}
+			],
+			'new york city': [
+				{
+					title: 'Times Square',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/New_York_City_%28New_York%2C_USA%29%2C_Times_Square-Duffy_Square_--_2012_--_6380.jpg/40px-New_York_City_%28New_York%2C_USA%29%2C_Times_Square-Duffy_Square_--_2012_--_6380.jpg'
+				},
+				{
+					title: 'NYC',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Washington_Crossing_the_Delaware_by_Emanuel_Leutze%2C_MMA-NYC%2C_1851.jpg/40px-Washington_Crossing_the_Delaware_by_Emanuel_Leutze%2C_MMA-NYC%2C_1851.jpg'
+				},
+				{
+					title: 'Manhattan',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/New_York_City_at_night_HDR.jpg/40px-New_York_City_at_night_HDR.jpg'
+				},
+				{
+					title: 'High line',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/London_Terrace_NY1.jpg/40px-London_Terrace_NY1.jpg'
+				},
+				{
+					title: 'Hotel',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/New_York_City_at_night_HDR.jpg/40px-New_York_City_at_night_HDR.jpg'
+				}
+			],
+			'van gogh': [
+				{
+					title: 'Sunflowers',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Van_Gogh_Sunflowers_Bracelets.jpg/40px-Van_Gogh_Sunflowers_Bracelets.jpg'
+				},
+				{
+					title: 'Iris',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Vincent_van_Gogh_-_Irises_%281889%29.jpg/40px-Vincent_van_Gogh_-_Irises_%281889%29.jpg'
+				},
+				{
+					title: 'Vincent',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg/40px-Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg'
+				},
+				{
+					title: 'Stolen',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Van_Gogh_-_The_Parsonage_Garden_at_Nuenen.jpg/40px-Van_Gogh_-_The_Parsonage_Garden_at_Nuenen.jpg'
+				},
+				{
+					title: 'Paintings',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg/40px-Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg'
+				}
+			],
+			'vincent van gogh': [
+				{
+					title: 'Starry night',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Vincent_van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/40px-Vincent_van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg'
+				},
+				{
+					title: 'Sunflowers',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Sunflowers_National_Gallery.jpg/40px-Sunflowers_National_Gallery.jpg'
+				},
+				{
+					title: 'Self portrait',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg/40px-Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg'
+				},
+				{
+					title: 'Paintings',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg/40px-Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg'
+				},
+				{
+					title: 'Museum',
+					thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg/40px-Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg'
+				}
+			]
+		}[ this.state.term.toLowerCase() ] || []
 	};
 };
 

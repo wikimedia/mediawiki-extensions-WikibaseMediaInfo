wikimedia/mediawiki-extensions-WikibaseMediaInfo

commit 1827c7a4f583182031ce7818413b037e65bdd575

Use MediaWiki's TitleFactory, instead of Wikibase client's
Change-Id: I39d26d1fb1469c6fad9fabc4344267db9fa32714

src/Services/FilePageLookup.php
@@ -3,7 +3,7 @@
 namespace Wikibase\MediaInfo\Services;
 
 use Title;
-use Wikibase\Client\Store\TitleFactory;
+use TitleFactory;
 use Wikibase\Lib\Store\StorageException;
 use Wikibase\MediaInfo\DataModel\MediaInfoId;
 

tests/phpunit/mediawiki/Content/MissingMediaInfoHandlerTest.php
@@ -7,7 +7,7 @@ use Language;
 use ParserOutput;
 use RequestContext;
 use Title;
-use Wikibase\Client\Store\TitleFactory;
+use TitleFactory;
 use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\DataModel\Services\EntityId\EntityIdComposer;
 use Wikibase\Lib\Store\StorageException;

tests/phpunit/mediawiki/Services/FilePageLookupTest.php
@@ -3,7 +3,7 @@
 namespace Wikibase\MediaInfo\Tests\MediaWiki\Services;
 
 use Title;
-use Wikibase\Client\Store\TitleFactory;
+use TitleFactory;
 use Wikibase\Lib\Store\StorageException;
 use Wikibase\MediaInfo\DataModel\MediaInfoId;
 use Wikibase\MediaInfo\Services\FilePageLookup;

src/Services/MediaInfoServiceWiring.php
@@ -1,7 +1,6 @@
 <?php
 
 use MediaWiki\MediaWikiServices;
-use Wikibase\Client\Store\TitleFactory;
 use Wikibase\MediaInfo\Content\MediaInfoHandler;
 use Wikibase\MediaInfo\Content\MissingMediaInfoHandler;
 use Wikibase\MediaInfo\DataModel\MediaInfo;

commit c2e3ee05e875503c5838b64a846f77229dd10b25

Follow-up 1827c7a: Ensure inNamespace() is called only on Title object
This was caused by 1827c7a, where the Wikibase Client's TitleFactory was just
replaced with core's TitleFactory, but they were not equal.

Precisely, Wikibase Client's returns Title or throws StorageException, while
core's return Title or null. FilePageLookup.php makes a call to inNamespace()
inside the try-block, and relied on the behavior of exception in such block
(i.e., an exception will force the inNamespace() call to be skipped). But the
switch to core's TitleFactory made the call return only Title and null, and
subsequent call to the inNamespace() method continued unconditionally, both
on Title and on null.

Bug: T253792
Change-Id: Ic9e8a211f3505c7f0f151a716df56cd2fde787a7

src/Services/FilePageLookup.php
@@ -4,7 +4,6 @@ namespace Wikibase\MediaInfo\Services;
 
 use Title;
 use TitleFactory;
-use Wikibase\Lib\Store\StorageException;
 use Wikibase\MediaInfo\DataModel\MediaInfoId;
 
 /**
@@ -34,17 +33,16 @@ class FilePageLookup {
 	public function getFilePage( MediaInfoId $mediaInfoId ) {
 		$pageId = $mediaInfoId->getNumericId();
 
-		try {
-			$filePageTitle = $this->titleFactory->newFromID( $pageId );
+		$filePageTitle = $this->titleFactory->newFromID( $pageId );
 
-			if ( $filePageTitle->inNamespace( NS_FILE ) ) {
-				return $filePageTitle;
-			}
-		} catch ( StorageException $ex ) {
-			// no such page
+		if ( !$filePageTitle ) {
+			return null;
+		}
+
+		if ( $filePageTitle->inNamespace( NS_FILE ) ) {
+			return $filePageTitle;
 		}
 
 		return null;
 	}
-
 }

tests/phpunit/mediawiki/Content/MissingMediaInfoHandlerTest.php
@@ -10,7 +10,6 @@ use Title;
 use TitleFactory;
 use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\DataModel\Services\EntityId\EntityIdComposer;
-use Wikibase\Lib\Store\StorageException;
 use Wikibase\MediaInfo\Content\MissingMediaInfoHandler;
 use Wikibase\MediaInfo\DataModel\MediaInfoId;
 use Wikibase\MediaInfo\Services\FilePageLookup;
@@ -47,7 +46,7 @@ class MissingMediaInfoHandlerTest extends \PHPUnit\Framework\TestCase {
 						$title->resetArticleID( $pageId );
 						return $title;
 					default:
-						throw new StorageException( 'No such page id: ' . $pageId );
+						return null;
 				}
 			} ) );
 

tests/phpunit/mediawiki/Services/FilePageLookupTest.php
@@ -4,7 +4,6 @@ namespace Wikibase\MediaInfo\Tests\MediaWiki\Services;
 
 use Title;
 use TitleFactory;
-use Wikibase\Lib\Store\StorageException;
 use Wikibase\MediaInfo\DataModel\MediaInfoId;
 use Wikibase\MediaInfo\Services\FilePageLookup;
 
@@ -37,7 +36,7 @@ class FilePageLookupTest extends \PHPUnit\Framework\TestCase {
 						$title->resetArticleID( $pageId );
 						return $title;
 					default:
-						throw new StorageException( 'No such page id: ' . $pageId );
+						return null;
 				}
 			} ) );
 
1978bba766f0b9525065ecc6fa9bd378b3f3cbb5
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-06-06 00:25:19 +0200

    Add integration test for watchlist behavior
    This adds an integration test for an annoying bug that has plagued
    WikibaseMediaInfo, though the fix is in Wikibase: when a user’s
    preferences said to watch newly created pages but not edited pages,
    creating the structured data part of a file would previously add that
    file to the watchlist even though the file was not newly created.
    
    Bug: T216369
    Change-Id: I480f96f2ea2a8bf86cf8975dd9f77aa644b84ef0
    Depends-On: Ib19df094e8d1ceeb15df9ff7ecb92d277390ccaa

---
+++ tests/phpunit/integration/WatchlistTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Wikibase\MediaInfo\Tests\Integration;
+
+use MediaWiki\MediaWikiServices;
+use Title;
+use Wikibase\MediaInfo\DataModel\MediaInfo;
+use Wikibase\Repo\WikibaseRepo;
+use Wikipage;
+
+/**
+ * What's tested:
+ * - set watch settings to watch created, but not edited pages
+ * - upload a file (as a different user that is not used again)
+ * - add a caption to it, this creates a new entity but should not watch the page
+ * - change watch settings to watch edited pages too
+ * - add another caption to it, this should now watch the page
+ *
+ * @group medium
+ * @group upload
+ * @group Database
+ * @coversNothing
+ */
+class WatchlistTest extends WBMIApiTestCase {
+
+	private $editor;
+
+	protected function setUp(): void {
+		parent::setUp();
+		$this->editor = self::$users['wbeditor']->getUser();
+	}
+
+	private function addCaption( string $entityId, string $language, string $caption ) {
+		$this->doApiRequestWithToken(
+			[
+				'action' => 'wbsetlabel',
+				'id' => $entityId,
+				'language' => $language,
+				'value' => $caption,
+				'bot' => 1,
+			],
+			null,
+			$this->editor
+		);
+	}
+
+	public function testWatchlist() {
+		$userOptionsManager = MediaWikiServices::getInstance()->getUserOptionsManager();
+		$userOptionsManager->setOption( $this->editor, 'watchdefault', 0 );
+		$userOptionsManager->setOption( $this->editor, 'watchcreations', 1 );
+
+		$testFileTitle = Title::newFromText( $this->uploadRandomImage() );
+		$testFilePage = WikiPage::factory( $testFileTitle );
+
+		$pageId = $testFilePage->getId();
+		$entityIdComposer = WikibaseRepo::getDefaultInstance()->getEntityIdComposer();
+		$entityId = $entityIdComposer->composeEntityId(
+			'',
+			MediaInfo::ENTITY_TYPE,
+			$pageId
+		)->getSerialization();
+
+		$this->addCaption( $entityId, 'en', 'a caption' );
+		$this->assertFalse(
+			$this->editor->isWatched( $testFileTitle ),
+			'page should not be watched because no new page was created'
+		);
+
+		$userOptionsManager->setOption( $this->editor, 'watchdefault', 1 );
+		$this->addCaption( $entityId, 'de', 'eine Kurzbeschreibung' );
+		$this->assertTrue(
+			$this->editor->isWatched( $testFileTitle ),
+			'page should be watched because an edit was made'
+		);
+	}
+
+}

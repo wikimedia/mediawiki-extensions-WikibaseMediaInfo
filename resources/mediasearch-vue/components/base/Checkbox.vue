682a92b887c5df96167769546ef9449b9aa782b3
annet <atomasevich@wikimedia.org>
2021-03-09 15:05:08 -0500

    Add checkbox and radio base components
    This patch adds reusable Checkbox and Radio components that match OOUI
    widget styles and functionality, while using Vue paradigms for data
    handling. See the binaryInput mixin for documentation.
    
    Since these components have a lot in common, shared code lives in a
    mixin, and there are some shared styles that have been separated out
    to avoid code duplication.
    
    Bug: T276260
    Change-Id: I7dc33ca69d79149d5656591884d4dcf3f8e95f6a

---
+++ resources/mediasearch-vue/components/base/Checkbox.vue
@@ -0,0 +1,38 @@
+<template>
+	<label
+		ref="label"
+		class="wbmi-checkbox"
+		:aria-disabled="disabled"
+		@click="onClick"
+		@keydown.prevent.enter="$refs.label.click"
+	>
+		<input
+			ref="input"
+			v-model="computedValue"
+			class="wbmi-checkbox__input"
+			type="checkbox"
+			:value="inputValue"
+			:aria-disabled="disabled"
+			:disabled="disabled"
+		>
+		<span class="wbmi-checkbox__icon" :aria-disabled="disabled"></span>
+		<span class="wbmi-checkbox__spacer"></span>
+		<span class="wbmi-checkbox__label-text">
+			<slot></slot>
+		</span>
+	</label>
+</template>
+
+<script>
+var binaryInput = require( './mixins/binaryInput.js' );
+
+/**
+ * Checkbox input.
+ */
+// @vue/component
+module.exports = {
+	name: 'WbmiCheckbox',
+
+	mixins: [ binaryInput ]
+};
+</script>

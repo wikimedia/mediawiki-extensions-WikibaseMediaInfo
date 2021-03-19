547e12a1a9a1099e4274290123cffe32586dc444
Addshore <addshorewiki@gmail.com>
2021-03-19 10:49:26 +0000

    Revert "Temporarily remove type hinting"
    This reverts commit 7cc3e47c405929512b707671b17ce9f900b5ef60.
    
    Reason for revert: We are not going to do this just yet
    
    Change-Id: I458d93b30230a1f14f747772b629a697d5b16e58

--- WikibaseMediaInfo.entitytypes.php
+++ WikibaseMediaInfo.entitytypes.php
@@ -14,6 +14,7 @@
  */
 
 use MediaWiki\MediaWikiServices;
+use Wikibase\DataAccess\SingleEntitySourceServices;
 use Wikibase\DataModel\DeserializerFactory;
 use Wikibase\DataModel\Entity\EntityDocument;
 use Wikibase\DataModel\Entity\EntityId;
@@ -269,7 +270,7 @@ return [
 				)
 			);
 		},
-		Def::PREFETCHING_TERM_LOOKUP_CALLBACK => function ( $services ) {
+		Def::PREFETCHING_TERM_LOOKUP_CALLBACK => function ( SingleEntitySourceServices $services ) {
 			return new MediaInfoPrefetchingTermLookup( $services->getEntityRevisionLookup() );
 		},
 		Def::ENTITY_ID_LOOKUP_CALLBACK => function () {

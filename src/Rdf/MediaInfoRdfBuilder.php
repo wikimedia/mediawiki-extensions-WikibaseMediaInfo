b00021823f689d2313ba2fe92af2b8f74c258f8a
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2021-05-26 15:17:30 +0200

    Introduce new MediaInfoRdfBuilder
    This class glues together all other builders needed
    for a media info entity to be written to the rdf
    output.
    
    Bug: T282685
    Change-Id: Ib2f4261137ae79f7d2924a1d88c71a9690979c7f

---
+++ src/Rdf/MediaInfoRdfBuilder.php
@@ -0,0 +1,90 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace Wikibase\MediaInfo\Rdf;
+
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\Repo\Rdf\EntityRdfBuilder;
+use Wikibase\Repo\Rdf\FullStatementRdfBuilder;
+use Wikibase\Repo\Rdf\FullStatementRdfBuilderFactory;
+use Wikibase\Repo\Rdf\RdfProducer;
+use Wikibase\Repo\Rdf\TermsRdfBuilder;
+use Wikibase\Repo\Rdf\TruthyStatementRdfBuilder;
+use Wikibase\Repo\Rdf\TruthyStatementRdfBuilderFactory;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class MediaInfoRdfBuilder implements EntityRdfBuilder {
+
+	/**
+	 * @var TruthyStatementRdfBuilder
+	 */
+	private $truthyStatementRdfBuilder;
+
+	/**
+	 * @var TermsRdfBuilder
+	 */
+	private $termsRdfBuilder;
+
+	/**
+	 * @var FullStatementRdfBuilder
+	 */
+	private $fullStatementRdfBuilder;
+
+	/**
+	 * @var MediaInfoSpecificComponentsRdfBuilder
+	 */
+	private $mediaInfoSpecificComponentsRdfBuilder;
+
+	public function __construct(
+		int $flavorFlags,
+		TermsRdfBuilder $termsRdfBuilder,
+		TruthyStatementRdfBuilderFactory $truthyStatementRdfBuilderFactory,
+		FullStatementRdfBuilderFactory $fullStatementRdfBuilderFactory,
+		MediaInfoSpecificComponentsRdfBuilder $mediaInfoSpecificComponentsRdfBuilder
+	) {
+		if ( $flavorFlags & RdfProducer::PRODUCE_TRUTHY_STATEMENTS ) {
+			$this->truthyStatementRdfBuilder = $truthyStatementRdfBuilderFactory->getTruthyStatementRdfBuilder(
+				$flavorFlags
+			);
+		}
+
+		if ( $flavorFlags & RdfProducer::PRODUCE_ALL_STATEMENTS ) {
+			$fullStatementRdfBuilder = $fullStatementRdfBuilderFactory->getFullStatementRdfBuilder(
+				$flavorFlags
+			);
+			$this->fullStatementRdfBuilder = $fullStatementRdfBuilder;
+		}
+
+		$this->termsRdfBuilder = $termsRdfBuilder;
+		$this->mediaInfoSpecificComponentsRdfBuilder = $mediaInfoSpecificComponentsRdfBuilder;
+	}
+
+	public function addEntity( EntityDocument $entity ) {
+		if ( $this->truthyStatementRdfBuilder ) {
+			$this->truthyStatementRdfBuilder->addEntity( $entity );
+		}
+
+		if ( $this->fullStatementRdfBuilder ) {
+			$this->fullStatementRdfBuilder->addEntity( $entity );
+		}
+
+		$this->termsRdfBuilder->addEntity( $entity );
+		$this->mediaInfoSpecificComponentsRdfBuilder->addEntity( $entity );
+	}
+
+	/**
+	 * Map some aspect of an Entity to the RDF graph, as it should appear in the stub
+	 * representation of an entity.
+	 *
+	 * The implementation of this method will often be empty, since most aspects of an entity
+	 * should not be included in the stub representation. Typically, the stub only contains
+	 * basic type information and labels, for use by RDF modelling tools.
+	 *
+	 * @param EntityDocument $entity the entity to output.
+	 */
+	public function addEntityStub( EntityDocument $entity ) {
+	}
+}

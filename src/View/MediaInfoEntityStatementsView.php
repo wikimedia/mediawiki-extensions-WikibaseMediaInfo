2ce6abd0904b17aa06e3f8757ee52db93dc00441
Matthias Mullie <git@mullie.eu>
2025-03-10 09:21:48 +0100

    SECURITY: Fix XSS vulnerability
    Attributes prefixed data-mw- are covered by Sanitizer.php and should be
    safe to use. Others are not safe, as they can also be used in Wikitext.
    
    For data-(mw-)property and data-(mw-)statements, fall back to the old
    attribute value for cached cases where the new ones are missing. This is
    hopefully relatively safe – Wikitext with custom values for these
    attributes may be able to cause confusing on-page behavior, but
    hopefully not code execution. To further safeguard this, make the
    selector more precise – this should hopefully cut out most
    user-generated content (though discussion on the task is inconclusive,
    as of this writing, whether it’s fully enough or not).
    
    For data-(mw-)formatvalue, don’t fall back to the old values. Values of
    this attribute are directly used as HTML and are thus highly unsafe; if
    we can’t use them, the wbformatvalue cache should just fall back to
    getting them from the API, which is slightly less efficient but safe.
    
    Bug: T387691
    Change-Id: Ie969a8cfeab0d4457417773fa884e271968e5657
    (cherry picked from commit 2cb392e5b22bdff08cd684124257005b22b622ce)

--- src/View/MediaInfoEntityStatementsView.php
+++ src/View/MediaInfoEntityStatementsView.php
@@ -257,9 +257,9 @@ class MediaInfoEntityStatementsView {
 		] );
 		$panel->setAttributes(
 			[
-				'data-property' => $propertyIdString,
-				'data-statements' => json_encode( $serializedStatements ),
-				'data-formatvalue' => json_encode( $formatValueCache ),
+				'data-mw-property' => $propertyIdString,
+				'data-mw-statements' => json_encode( $serializedStatements ),
+				'data-mw-formatvalue' => json_encode( $formatValueCache ),
 			]
 		);
 		return $panel;
@@ -345,11 +345,6 @@ class MediaInfoEntityStatementsView {
 
 		$statementDiv = new Tag( 'div' );
 		$statementDiv->addClasses( [ 'wbmi-item-container' ] );
-
-		$guid = $statement->getGuid();
-		if ( $guid !== null ) {
-			$statementDiv->setAttributes( [ 'data-guid' => $guid ] );
-		}
 
 		$mainSnakDiv = new Tag( 'div' );
 		$mainSnakDiv->addClasses( [ 'wbmi-entity-header' ] );
